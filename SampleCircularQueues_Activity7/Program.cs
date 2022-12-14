236ca408c763f1b31f5210542c46e629a36d974d
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-12-14 16:52:04 +0700

    This statement checks for the overflow condition

--- SampleCircularQueues_Activity7/Program.cs
+++ SampleCircularQueues_Activity7/Program.cs
@@ -11,5 +11,12 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+            }
+        }
     }
 }
